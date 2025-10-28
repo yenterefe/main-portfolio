a79bfa0e225f31b22e36841b00a162a6a1c2a53f
yenterefe <yenterefe@example.com>
2025-10-28 12:29:52 -0400

    feat: added mobile responsivess for images

--- Frontend/src/Components/Smithsonian.tsx
+++ Frontend/src/Components/Smithsonian.tsx
@@ -4,11 +4,11 @@ const Smithsonian = () => {
     return (
         <div>
             <div className="hover:scale-105">
-                <video className="animation-slide h-80 " src="#" controls
+                <video className="h-48 md:h-80" src="#" controls
                     poster="https://dummyimage.com/600x400/eeeeee/fff" />
             </div>
             <div className="text-center">
-                <p className="h-80 p-2 text-slate-700">Smithsonian XR Apps </p>
+                <p className="h-80 px-2 py-8  md:px-0 md:py-0 md:p-2 text-slate-700">Smithsonian XR Apps </p>
             </div>
         </div >
 
